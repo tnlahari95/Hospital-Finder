4abc91323eecfe0c4c2e84755b35ee55ac9d152c
lahari <tnlahari@gmail.com>
2020-10-10 19:55:05 -0500

    Added pages with styling quotient and content

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction1() 
+{
+window.location.assign("generalawareness.html");
+
+}
+function myFunction2() 
+{
+window.location.assign("hospitalfinder.html");
+
+}
+function myFunction3() 
+{
+window.location.assign("chicagometrics.html");
+
+}
+
+  
