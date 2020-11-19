2d819365f6229f2b99aedd5bae867d316cab02e3
Tom Johnson <tomajohnson21@gmail.com>
2020-11-19 16:45:40 -0600

    Fixed csjscode.js to correctly grab data for United States

--- js/csjscode.js
+++ js/csjscode.js
@@ -21,18 +21,18 @@ function init() {
 
   var apiusdata = ``
   $.get(url, function (apiusdata) {
-    console.log(apiusdata.Countries.Country, "United States of America")
+    console.log(apiusdata.Countries[181], "United States of America")
 
     apiusdata = `
-      <td>${apiusdata.Countries.Country.NewConfirmed}</td>
-      <td>${apiusdata.Countries.Country.TotalConfirmed}</td>
-      <td>${apiusdata.Countries.Country.NewDeaths}</td>
-      <td>${apiusdata.Countries.Country.TotalDeaths}</td>
-      <td>${apiusdata.Countries.Country.NewRecovered}</td>
-      <td>${apiusdata.Countries.Country.TotalRecovered}</td>
+      <td>${apiusdata.Countries[181].NewConfirmed}</td>
+      <td>${apiusdata.Countries[181].TotalConfirmed}</td>
+      <td>${apiusdata.Countries[181].NewDeaths}</td>
+      <td>${apiusdata.Countries[181].TotalDeaths}</td>
+      <td>${apiusdata.Countries[181].NewRecovered}</td>
+      <td>${apiusdata.Countries[181].TotalRecovered}</td>
       
       `
-    $("#apidata").html(apidata)
+    $("#apiusdata").html(apiusdata)
   });
 }
 
