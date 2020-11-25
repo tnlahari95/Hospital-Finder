2e047057dc0c507a5ff4f8f5044f40bd88304320
Emmanuel Thomas <ethomas14@luc.edu>
2020-11-25 15:06:57 -0600

    Added google maps api to hospital finder page to search for chicago hospitals. will need to enable finding lat and long by zip.

--- js/script.js
+++ js/script.js
@@ -75,3 +75,71 @@ function myFunction3()
 window.location.assign("covidMetrics.html");
 
 }
+
+
+// Initialize and add the map
+var map;
+
+function initMap() {
+    // Create the map.
+    var chicago = {
+        lat: 41.881832,
+        lng:  -87.623177
+    };
+    if (navigator.geolocation) {
+        try {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var chicago = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+            });
+        } catch (err) {
+
+        }
+    }
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: chicago,
+        zoom: 17
+    });
+
+    // Create the places service.
+    var service = new google.maps.places.PlacesService(map);
+
+    // Perform a nearby search.
+    service.nearbySearch({
+            location: chicago,
+            radius: 4000,
+            type: ['hospital']
+        },
+        function(results, status, pagination) {
+            if (status !== 'OK') return;
+
+            createMarkers(results);
+            getNextPage = pagination.hasNextPage && function() {
+                pagination.nextPage();
+            };
+        });
+}
+
+function createMarkers(places) {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+        var image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
+
+        var marker = new google.maps.Marker({
+            map: map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location
+        });
+        bounds.extend(place.geometry.location);
+    }
+    map.fitBounds(bounds);
+}
